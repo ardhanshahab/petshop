257b0a53b42cd113639228290eaff8bf9e5f0fce
Muhamad Ardhan Hadaina <65588203+ardhanshahab@users.noreply.github.com>
2024-05-10 12:42:31 +0700

    first commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,59 @@
+<!-- Footer -->
+<footer class="text-center text-lg-start bg-transparant text-muted ">
+  <!-- Section: Social media -->
+  <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+    <!-- Left -->
+    {{-- <div class="mdb-5 d-none d-lg-block" style="">
+      <span>Get connected with us on social networks:</span>
+    </div> --}}
+    <!-- Left -->
+
+    
+  </section>
+  <!-- Section: Social media -->
+
+  <!-- Section: Links  -->
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <!-- Content -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            <i class="fas fa-gem me-3"></i>Travelocat
+          </h6>
+          <p>
+            Selamat datang di Travelocat!
+            Platform online yang menjual berbagai kebutuhan hewan
+          </p>
+        </div>
+        <!-- Grid column -->
+
+       
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+          <p><i class="fas fa-home me-3"></i> Bandung</p>
+          <p>
+            <i class="fas fa-envelope me-3"></i>
+            sales@travelocat.com
+          </p>
+          <p><i class="fas fa-phone me-3"></i> + 628 1234 5678</p>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+  <!-- Section: Links  -->
+
+  <!-- Copyright -->
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+    © 2024 Copyright: Travelocat.
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
